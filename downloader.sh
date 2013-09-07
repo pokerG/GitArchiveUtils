ee0fb6db655be5e9a45375402f83044166a5ba38
liushuaikobe <liushuaikobe@gmail.com>
2013-09-07 22:46:16 +0800

    完成了下载一个月数据的脚本

---
+++ downloader.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+year=2012
+month=01
+# day=01
+if [ -z "$1" ]
+then
+	echo "Please tell me where to store these files."
+else
+	for day in $(seq 1 31)
+	do
+		for hour in $(seq 0 23)
+		do
+			if [ "$day" -lt "10" ]
+			then
+				wget -P "$1" http://data.githubarchive.org/"$year"-"$month"-0"$day"-"$hour".json.gz
+			else
+				wget -P "$1" http://data.githubarchive.org/"$year"-"$month"-"$day"-"$hour".json.gz
+			fi
+		done
+	done
+fi
