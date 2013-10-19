fa99c17e15471a2e25c218c1737fd9dfd3a6e8f1
Leiguang Hao <pokerfacehlg@gmail.com>
2013-10-19 19:46:14 +0800

    change method

--- WriteToMongo-go.go
+++ WriteToMongo-go.go
@@ -11,33 +11,20 @@ import (
 	"path/filepath"
 	"regexp"
 	"runtime"
-	"sync"
-	"sync/atomic"
+	//"sync"
+	//"sync/atomic"
 	"time"
 	//"labix.org/v2/mgo/bson"
 )
 
 const BUFSIZE int = 40000
 
-var chs []chan int
-var cnum int
-var currentNum int32
-
 func main() {
 	if len(os.Args) > 1 {
 		runtime.GOMAXPROCS(runtime.NumCPU())
-
 		t := time.Now()
-
-		chs = make([]chan int, 1)
-		cnum = 0
-
 		Tree(os.Args[1], 1)
-		for _, ch := range chs {
-			<-ch
-		}
 		fmt.Println("total time :", time.Since(t))
-
 	} else {
 		fmt.Println("Please input the Dir or file path")
 	}
@@ -57,33 +44,19 @@ func Tree(dirname string, curHier int) {
 			b := []byte(fileInfo.Name())
 			matched, _ := regexp.Match("[.](json.gz)$", b)
 			if matched {
-				if cnum >= len(chs) {
-					chs = append(chs, make(chan int))
-				}
-				chs[cnum] = make(chan int)
-				go UZip(filepath.Join(dirAbs, fileInfo.Name()), chs[cnum])
-				cnum += 1
-
+				UZip(filepath.Join(dirAbs, fileInfo.Name()))
 			}
-
 		}
-
 	}
 }
 
-func UZip(fpath string, ch chan int) {
-
-	for atomic.LoadInt32(&currentNum) > 5 {
-		time.Sleep(time.Second)
-	}
-
-	atomic.AddInt32(&currentNum, 1)
+func UZip(fpath string) {
 
 	fr, err := os.Open(fpath)
 	handleError(err)
 	defer fr.Close()
 
-	//fmt.Println(fr.Name())
+	fmt.Println(fr.Name())
 
 	gr, err := gzip.NewReader(fr)
 	handleError(err)
@@ -95,12 +68,10 @@ func UZip(fpath string, ch chan int) {
 	var num int = 0
 
 	for {
-
 		n, err := gr.Read(buf)
 		//fmt.Println(n)
 		//fmt.Println(buf)
 		data = append(data, buf[:n]...)
-
 		if err == io.EOF {
 			break
 		}
@@ -108,10 +79,10 @@ func UZip(fpath string, ch chan int) {
 		handleError(err)
 	}
 
-	WriteToMongo(fr.Name(), data, ch)
+	WriteToMongo(data)
 }
 
-func WriteToMongo(fname string, data []byte, ch chan int) {
+func WriteToMongo(data []byte) {
 	session, err := mgo.Dial("localhost:27017")
 	handleError(err)
 	defer session.Close()
@@ -123,30 +94,30 @@ func WriteToMongo(fname string, data []byte, ch chan int) {
 
 	sdata := reg.FindAllString(string(data), -1)
 
-	//fmt.Println(sdata)
+	chs := make([]chan int, len(sdata))
 
-	for _, s := range sdata {
-		var inter interface{}
-
-		err = json.Unmarshal([]byte(s), &inter)
-		handleError(err)
-		c := session.DB("testGoBig").C("Event")
-		err = c.Insert(inter)
-
+	for i, s := range sdata {
+		chs[i] = make(chan int)
+		go func(s string, i int) {
+			var inter interface{}
+			//fmt.Println("GOruntine!!", i)
+			err := json.Unmarshal([]byte(s), &inter)
+			handleError(err)
+			c := session.DB("testGoBig").C("Event")
+			err = c.Insert(inter)
+			chs[i] <- 1
+		}(s, i)
+		if i%5000 == 0 {
+			time.Sleep(time.Second)
+		}
 	}
 
-	lock := &sync.Mutex{}
-	lock.Lock()
-	fmt.Println(fname)
+	for _, ch := range chs {
+		<-ch
+	}
+
 	fmt.Println(len(sdata))
-	lock.Unlock()
-	atomic.AddInt32(&currentNum, -1)
-	runtime.Gosched()
-
 	handleError(err)
-
-	ch <- 1
-
 }
 
 func handleError(err error) {
